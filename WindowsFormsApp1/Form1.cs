f76adbc84434124124df02e9d2005238b10e9998
Bikmuhametov-Daniil-is-1-19 <danya.kovaleva29@yandex.ru>
2021-12-01 21:33:46 +0500

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        abstract class Komplektyushie<artikyl>
+        {
+            public int price;
+            public string godvipysk;
+            public artikyl artikyll;
+            public abstract void Display(ListBox listBox1);
+            public Komplektyushie(int pricee, string godvipyskk, artikyl art)
+            {
+                price = pricee;
+                godvipysk = godvipyskk;
+                artikyll = art;
+            }
+             class CPU : Komplektyushie
+            {
+                public int mhz;
+                public int cores;
+                public int streams;
+                public override void Display(ListBox listBox1);
+            } 
+
+
+
+           
+            
+
+        }
+    }
+}
